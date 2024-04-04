34cd6e8dc6a9d5946afd50eeab7609749ca0270c
ttwag <taowang@ucdavis.edu>
2024-04-04 09:12:43 -0700

    Remove CCS IDE files

--- lab/lab1/lab1-pt2/Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"/Applications/ti/lib/cc3200sdk_1.5.0/cc3200-sdk/driverlib/ccs/Release/driverlib.a" -llibc.a
-
