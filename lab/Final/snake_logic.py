31965e0aa03a51c3a6f453b28a59babe82ceff4d
ttwag <taowang@ucdavis.edu>
2024-06-01 17:32:41 -0700

    Update snake_logic.py

--- lab/Final/snake_logic.py
+++ lab/Final/snake_logic.py
@@ -1,5 +1,66 @@
 import pygame
 
+class ListNode:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    def insert_front(self, data):
+        new_Node = ListNode(data)
+        new_Node.prev = None
+        if self.length:
+            new_Node.next = self.head
+            self.head.prev = new_Node
+            self.head = new_Node
+        else:
+            new_Node.next = None
+            self.head = new_Node
+            self.tail = new_Node
+        self.length+=1
+    def delete_tail(self):
+        if self.length > 1:
+            self.length-=1
+            self.tail = self.tail.prev
+            self.tail.next = None
+        else:
+            self.length = 0
+            self.head = None
+            self.tail = None
+    def print_list(self):
+        node = self.head
+        while node:
+            print(node.data)
+            node = node.next
+class Snake:
+    def __init__(self, length, seg_size, init_pos):
+        self.seg_size = seg_size
+        self.body = DoublyLinkedList()
+        for i in range(length-1, -1, -1):
+            self.body.insert_front([init_pos + i * seg_size, init_pos])
+        self.snake_head = self.body.head
+        self.snake_tail = self.body.tail
+    def move(self, direction):
+        head_x = self.head.data[0]
+        head_y = self.head.data[1]
+        if direction == 0:
+            self.body.insert_front([head_x + self.seg_size, head_y])
+        elif direction == 1:
+            self.body.insert_front([head_x - self.seg_size, head_y])
+        elif direction == 2:
+            self.body.insert_front([head_x, head_y - self.seg_size])
+        else:
+            self.body.insert_front([head_x, head_y + self.seg_size])
+        self.body.delete_tail()
+    def self_collision(self):
+        pass
+
+
 # Open a window
 pygame.init()
 screen_width = 800
@@ -35,7 +96,7 @@ rect_y = 100
 
 # Direction: 0, 1, 2, 3 - Right, Left, Up, Down
 direction = 3
-snake_seg_size = 15
+snake_seg_size = 20
 snake_length = 15
 
 snake = []
@@ -51,6 +112,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    # End the game if snake's head hits the wall
     if (snake[0][0] >= screen_width - wall_thickness - snake_seg_size): 
         break
     if (snake[0][0] <= wall_thickness):
@@ -59,12 +121,13 @@ while running:
         break
     if (snake[0][1] <= wall_thickness):
         break
-
+    
     
     # Delete the old rectangle by drawing over it with a black rectangle
     for segment in snake:
         pygame.draw.rect(screen, BLACK, (segment[0], segment[1], snake_seg_size, snake_seg_size))
     
+    # Randomly generate new food if not have one already
 
     # Move the rectangle depending on the pressed key and previous direction
     keys = pygame.key.get_pressed()
@@ -84,7 +147,8 @@ while running:
         snake[0][2] = direction
         last_direction = direction
     
-
+    # Record the new position of the head and checks if there's collision with other segments
+    # If the new position 
     for segment in snake:
         curr_direction = segment[2]
         if curr_direction == 0:
